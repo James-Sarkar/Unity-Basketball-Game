a6f0b5ad1b7865476dd9f4eaba966632caf40cbd
James Sarkar <Nexus@nexuss-mbp-2.lan>
2017-11-25 20:10:27 -0500

    Added trail particle and a script for the basketball

---
+++ Assets/Project/Scripts/Ball.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public GameObject trailObject;
+
+	// Use this for initialization
+	void Start () {
+		trailObject.SetActive (false);
+	}
+
+	// Use this to active trail object
+	public void ActivateTrail () {
+		trailObject.SetActive (true);
+	}
+}
